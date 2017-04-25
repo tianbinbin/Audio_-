316523fecd34b92c206de6bcbf6246d1a3bd9d22
田彬彬 <tbb@tianbinbindeMac-mini.local>
2017-04-25 08:20:03 +0800

    创建工程文件

---
+++ Audio_tool/Audio_tool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Audio_tool
+//
+//  Created by 田彬彬 on 2017/4/25.
+//  Copyright © 2017年 田彬彬. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
